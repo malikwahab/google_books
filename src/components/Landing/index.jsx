2718a9e7672062f04f9feb4003d47dc6e000e42e
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2018-07-30 01:50:55 +0100

    Setup folder structure

---
+++ src/components/Landing/index.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+const Landing = () => <div>Home is here</div>
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, useRouterHistory } from 'react-router';
+import { render } from 'react-dom';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import Landing from './components/Landing';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const appHistory = useRouterHistory(createBrowserHistory)({ queryKey: false });
+
+const routes = (
+  <Router history={appHistory}>
+    <Route path="/" component={Landing} />
+  </Router>
 );
+
+render(routes, document.getElementById('root'));
